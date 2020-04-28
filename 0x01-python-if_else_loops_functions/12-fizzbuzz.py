a906976da8bf127ada15af9d02a61a32d6f90118
Eng-RonnieBM <ronniealberto@gmail.com>
2020-04-28 17:22:18 -0500

    fixing

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -11,32 +11,3 @@ def fizzbuzz():
             print("{:d}".format(i), end= ' ');
         if (i != 100):
             print("{}".format(""), end='')
-
-'''
-int main(void)
-{
-    int i;
-
-    for (i = 1; i <= 100 ; i++)
-    {
-        if ((i % 3 == 0) && (i % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        printf("%d", i);
-        if (i != 100)
-        printf(" ");
-    }
-    printf("\n");
-    return (0);
-}
-'''
