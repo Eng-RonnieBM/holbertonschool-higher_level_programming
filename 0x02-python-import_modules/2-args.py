b87dfc318afba2209104d6a303547c275be276b9
Eng-RonnieBM <ronniealberto@gmail.com>
2020-04-30 13:35:06 -0500

    uploading task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+argc = len(sys.argv)
+if argc > 1:
+    print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, sys.argv[i]))
+else:
+    print("0 arguments.")
