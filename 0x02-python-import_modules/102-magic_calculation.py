831de25614bce3e0e7a995df50850597d13c0b01
Eng-RonnieBM <ronniealberto@gmail.com>
2020-04-30 18:45:19 -0500

    uploading task8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = a + b
+        for i in range(4, 6):
+            c = c + i
+        return (c)
+    else:
+        return (a - b)
