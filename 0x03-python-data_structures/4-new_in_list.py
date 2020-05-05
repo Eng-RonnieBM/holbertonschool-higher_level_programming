2656366461a0d37f4abaf6828629ef0954ef0b47
Eng-RonnieBM <ronniealberto@gmail.com>
2020-05-05 13:14:04 -0500

    fixing if

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if (my_list is None):
+        return ()
     if 0 < idx > len(my_list):
         return (my_list)
     copy = list(my_list)
