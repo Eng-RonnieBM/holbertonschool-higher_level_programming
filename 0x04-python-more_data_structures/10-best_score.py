6a3bc941f6a319109fa2b605b64dfd1b657b524b
Eng-RonnieBM <ronniealberto@gmail.com>
2020-05-07 16:51:20 -0500

    uploading task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    condition = True
+    for person in a_dictionary:
+        if condition:
+            name = person
+            score = a_dictionary[person]
+            condition = False
+        if a_dictionary[person] > score:
+            score = a_dictionary[person]
+            name = person
+    return name
